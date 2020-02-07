129100ba3fce9cffda6dbedbad42146a1172047f
deolu18 <41289746+deolu18@users.noreply.github.com>
2020-02-07 17:42:13 +0000

    Update jenkins-ec2.tf

--- hello-World/ci/Jenkins/jenkins-ec2.tf
+++ hello-World/ci/Jenkins/jenkins-ec2.tf
@@ -1,42 +1,64 @@
-data "template_file" "user_data_master" {
-  template = "${file("user-data/jenkins.tpl")}"
-
-  vars = {
-    efs_dns_name    = "${var.efs_dns_name}"
-    ebs_device_name = "${var.ebs_device_name}"
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  tags = {
+    Name = "main"
   }
 }
 
-data "aws_availability_zones" "available" {}
-
-resource "aws_key_pair" "jenkins_stack_key_pair" {
-  key_name   = "${var.workload }_${var.account}"
-  public_key = "${var.public_key}"
-}
-
-# jenkins master instance
-resource "aws_instance" "jenkins_stack_master" {
-  ami                    = "${data.aws_ami.jenkins.id}"
-  key_name               = "${var.workload }_${var.account}"
-  vpc_security_group_ids = data.aws_security_groups.jenkins_main_instance_sg.ids
-  subnet_id              = data.aws_subnet_ids.private.ids
-  iam_instance_profile   = "${data.aws_iam_instance_profile.jenkins_instance_profile.name}"
-  user_data              = "${data.template_file.user_data_master.rendered}"
+# Subnets
+resource "aws_subnet" "main-public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1a"
 
   tags = {
-    Name     = "${var.workload}_${var.account}"
-    account  = "${var.account }"
-    workload = "${var.workload}"
+    Name = "main-public-1"
   }
 }
 
-resource "aws_elb_attachment" "jenkins_elb_instance_attachment" {
-  elb      = "${data.aws_elb.jenkins_main_elb.id}"
-  instance = "${aws_instance.jenkins_stack_master.id}"
+data "template_file" "user_data_master" {
+  template = "${file("user-data/jenkins.tpl")}"
+  vars = {
+    DEVICE          = var.instance_device_storage
+    JENKINS_VERSION = var.jenkins_vers
+  }
 }
 
-resource "aws_volume_attachment" "jenkins-data-attachment" {
-  device_name = "${var.ebs_device_name}"
-  volume_id   = "${data.aws_ebs_volume.jenkins_ebs.id}"
-  instance_id = "${aws_instance.jenkins_stack_master.id}"
+resource "aws_subnet" "main-private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1a"
+
+  tags = {
+    Name = "${var.workload}"
+  }
+}
+
+resource "aws_instance" "hellow-jenkins" {
+  ami                    = var.AMIS[var.AWS_REGION]
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main-public.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  key_name               = aws_key_pair.mykeypair.key_name
+  user_data              = "${data.template_file.user_data_master.rendered}"
+}
+
+resource "aws_ebs_volume" "jenkins-store" {
+  availability_zone = "eu-west-1a"
+  size              = 10
+  type              = "gp2"
+  tags = {
+    Name = "jenkins-store"
+  }
+}
+
+resource "aws_volume_attachment" "jenkins-store-attachment" {
+  device_name  = var.instance_device_storage
+  volume_id    = aws_ebs_volume.jenkins-store.id
+  instance_id  = aws_instance.hellow-jenkins.id
 }
