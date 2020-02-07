74282bbc82534f10891336781619ca8b68cd2ef2
deolu18 <41289746+deolu18@users.noreply.github.com>
2020-02-07 17:38:28 +0000

    Update var.tf

--- hello-World/ci/Jenkins/var.tf
+++ hello-World/ci/Jenkins/var.tf
@@ -1,13 +1,34 @@
-variable workload_name {}
-variable account {}
-variable region {}
-variable public_key {}
-variable instance {}
-variable prefix {}
-variable "region" {}
-variable efs_sg_name {}
-variable efs_dns_name {}
-variable ebs_device_name {}
-variable master_ebs_id {}
+variable region{
+  default = "eu-west-1"
+}
+variable account{
+  default = ""
+}
+variable workload{
+  default = "helloApplication"
+}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "private_key_word" {
+  default = "mykey"
+}
 
+variable "public_key_word" {
+  default = "mykey.pub"
+}
 
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = ""
+  }
+}
+
+variable "instance_device_storage" {
+  default = "/dev/xvdh"
+}
+
+variable "jenkins_vers" {
+  default = "2.204.1"
+}
