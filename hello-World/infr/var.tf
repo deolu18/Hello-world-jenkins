8d7d8f4c39f73422ec1d5bb5687d84b40e3e1089
deolu18 <41289746+deolu18@users.noreply.github.com>
2020-02-07 17:35:01 +0000

    Update var.tf

--- hello-World/infr/var.tf
+++ hello-World/infr/var.tf
@@ -1,22 +1,15 @@
-variable "workload" {}
-
-variable "vpc_prefix" {}
-variable "hello-world-task-name"
-variable "region" {}
-variable "loggroup_name" {}
-variable "cluster_name" {}
-variable "role_name" {}
-variable "account_number" {}
-variable "account" {}
-variable "public_elb_sg_name" {}
-variable "instance_sg_name" {}
-variable "allowed_cidrs" {}
-variable "egress_cidrs" {
-  default = "0.0.0.0/0"
+variable region{
+  default = "eu-west-1"
 }
-
-variable "private_network_cidr" {
-  default = "10.0.0.0/16"
+variable account_number{
+  default = ""
 }
-
-
+variable ecr_image_repo_name{
+  default = "hello-world"
+}
+variable image_tag{
+  default = "latest"
+}
+variable aws_log_group_hello_world{
+  default = "/aws/ecs/logs/hello"
+}
